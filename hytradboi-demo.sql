4ca917b8cb708ede5108d1dddaf7d904fcdf8a12
Samuel Tschiedel <mukaobr@gmail.com>
2022-10-16 14:30:07 -0300

    style: use pg_format

--- hytradboi-demo.sql
+++ hytradboi-demo.sql
@@ -1,54 +1,54 @@
-with hn_stories as (
-select
-  ((regexp_match(url, '^.*//github.com/([^/]+/[^/]+)'))[1]) as repository_full_name,
-  score
-from
-  hackernews_top
-where
-  score >= 10
-  and type = 'story'
-  and url ilike 'https://github.com/%'
-limit 10
+WITH hn_stories AS (
+    SELECT
+        ((regexp_match(url, '^.*//github.com/([^/]+/[^/]+)'))[1]) AS repository_full_name,
+        score
+    FROM
+        hackernews_top
+    WHERE
+        score >= 10
+        AND type = 'story'
+        AND url ILIKE 'https://github.com/%'
+    LIMIT 10
+),
+workflows AS (
+    SELECT
+        gw.name,
+        gw.path,
+        workflow_file_content_json AS json_content
+    FROM
+        hn_stories,
+        github_workflow AS gw
+    WHERE
+        gw.repository_full_name = hn_stories.repository_full_name
+),
+jobs AS (
+    SELECT
+        key AS name,
+        value
+    FROM
+        workflows,
+        jsonb_each(workflows.json_content -> 'jobs')
+),
+steps AS (
+    SELECT
+        steps.value -> 'name' AS name,
+        steps.value -> 'uses' AS action
+    FROM
+        jobs,
+        jsonb_array_elements(jobs.value -> 'steps') AS steps
 )
-, workflows as (
-select
-  gw.name,
-  gw.path,
-  workflow_file_content_json as json_content
-from
-  hn_stories,
-  github_workflow as gw
-where
-  gw.repository_full_name = hn_stories.repository_full_name
-)
-, jobs as (
-select
-  key as name,
-  value
-from
-  workflows,
-  jsonb_each(workflows.json_content -> 'jobs')
-)
-, steps as (
-select
-  steps.value -> 'name' as name,
-  steps.value -> 'uses' as action
-from
-  jobs,
-  jsonb_array_elements(jobs.value -> 'steps') as steps
-)
-select distinct on (steps.action)
-  hn_stories.score as hn_score,
-  concat('https://github.com/', hn_stories.repository_full_name) as repo_url,
-  workflows.name as workflow,
-  workflows.path as workflow_path,
-  jobs.name as job,
-  steps.name as step,
-  steps.action
-from
-  steps,
-  jobs,
-  workflows,
-  hn_stories
-where
-  steps.action is not null
+SELECT DISTINCT ON (steps.action)
+    hn_stories.score AS hn_score,
+    concat('https://github.com/', hn_stories.repository_full_name) AS repo_url,
+    workflows.name AS workflow,
+    workflows.path AS workflow_path,
+    jobs.name AS job,
+    steps.name AS step,
+    steps.action
+FROM
+    steps,
+    jobs,
+    workflows,
+    hn_stories
+WHERE
+    steps.action IS NOT NULL
